8166a37cbbfe3b0ab1c2c8afc94aaeaa3072141c
周威豪 <161098301+Pattchou0915@users.noreply.github.com>
2024-03-09 14:57:17 +0800

    Update 火車站進出人口數_2.sql

--- 火車站進出人口數_2.sql
+++ 火車站進出人口數_2.sql
@@ -8,3 +8,14 @@ FROM gate_count LEFT JOIN stations ON 站點編號=編號;
 SELECT DISTINCT 名稱
 FROM gate_count LEFT JOIN stations ON 站點編號=編號
 WHERE 地址 LIKE '基隆市%'
+
+/*取出基隆市2022年3月1日資料*/
+SELECT 日期,進站人數,出站人數,名稱,地名,地址,youbike
+FROM gate_count LEFT JOIN stations ON 站點編號=編號
+WHERE 名稱 = '基隆' AND 日期 = '2022-03-01';
+
+/*取出基隆市2022年3月份資料，時間由小到大排序*/
+SELECT 日期,進站人數,出站人數,名稱,地名,地址,youbike
+FROM gate_count LEFT JOIN stations ON 站點編號=編號
+WHERE 名稱 = '基隆' AND (日期 BETWEEN '2022-03-01' AND '2022-03-31')
+ORDER BY 日期 ASC;
