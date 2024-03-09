dc642588674781e7bcb26f02c34cc36103123ebe
周威豪 <161098301+Pattchou0915@users.noreply.github.com>
2024-03-09 11:40:10 +0800

    Update 關聯式資料庫.sql

--- 關聯式資料庫.sql
+++ 關聯式資料庫.sql
@@ -1,20 +1,17 @@
-DROP TABLE IF EXISTS 客戶;
 DROP TABLE IF EXISTS 聯絡人;
+DROP TABLE IF EXISTS 客戶;
 
-	
-/*建立客戶資料表*/
-	CREATE TABLE IF NOT EXISTS 客戶(
+
+CREATE TABLE IF NOT EXISTS 客戶(
 	客戶_id SERIAL,
 	客戶名稱 VARCHAR(255) NOT NULL,
 	PRIMARY KEY(客戶_id)
-
 );
 
-/*建立聯絡人資訊*/	
 CREATE TABLE IF NOT EXISTS 聯絡人(
 	聯絡人id SERIAL,
 	客戶id INT,
-	聯絡姓名 VARCHAR(255) NOT NULL,
+	聯絡人姓名 VARCHAR(255) NOT NULL,
 	電話 VARCHAR(15),
 	郵件 VARCHAR(100),
 	PRIMARY KEY(聯絡人id),
@@ -23,18 +20,18 @@ CREATE TABLE IF NOT EXISTS 聯絡人(
 	
 );
 
-/*INSERT*/		
 INSERT INTO 客戶(客戶名稱)
-VALUES ('遠傳電信'),
-	   ('台灣大車隊')
+VALUES('遠傳電信'),
+      ('台灣大車隊');
 
-INSERT INTO 聯絡人(客戶id,聯絡姓名,電話,郵件)
+INSERT INTO 聯絡人(客戶id,聯絡人姓名,電話,郵件)
 VALUES (1,'John Doe','(408)-111-1234','john.doe@bluebird.dev'),
 	   (1,'Jane Doe','(408)-111-1235','jane.doe@bluebird.dev'),
 	   (2,'David Wright','(408)-222-1234','david.wright@dolphin.dev');
 
+
 DELETE FROM 客戶
-WHERE 客戶_id = 1
+WHERE 客戶_id = 1;
 
 SELECT * FROM 客戶;
 SELECT * FROM 聯絡人;
